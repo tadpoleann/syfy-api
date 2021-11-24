a590b2b5727f6c5b0ca7f9859536f67ec26254e6
tadpoleann <laesybab@gmail.com>
2021-11-24 13:17:56 -0800

    Added cors

--- index.js
+++ index.js
@@ -4,31 +4,43 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 // const uuid = require('uuid');
 const Models = require("./models.js");
+const passport = require("passport");
+require("./passport");
+const cors = require("cors");
 
 // call models from model.js
 const Movies = Models.Movie;
 const Users = Models.User;
+let allowedOrigins = ["http://localhost:8080", "http://testsite.com"];
 
-mongoose.connect("mongodb://localhost:27017/syfyDB", {
+mongoose.connect("mongodb://localhost:27017/mySyfyDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 const app = express();
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        let message =
+          "The CORS policy for this application doesn’t allow access from origin " +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
-//log requests to terminal
-app.use(morgan("common"));
-//serve "documentation.html" from "/public" folder
-app.use(express.static("public"));
 app.use(bodyParser.json());
-
 let auth = require("./auth.js")(app);
-
-const passport = require("passport");
-require("./passport");
+app.use(express.static("public"));
+app.use(morgan("common"));
 
 // GET requests
-//express route located at the endpoint "/"
+
 app.get("/", (req, res) => {
   res.send("Welcome to my Syfy Films database!");
 });
