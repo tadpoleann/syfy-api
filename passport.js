a5945a9ab2db6ff749d0ac661b43a9d21d4d9f2c
tadpoleann <laesybab@gmail.com>
2021-11-24 13:19:33 -0800

    validated password

--- passport.js
+++ passport.js
@@ -28,6 +28,11 @@ passport.use(
           });
         }
 
+        if (!user.validatePassword(password)) {
+          console.log("Incorrect Password");
+          return callback(null, false, { message: "Incorrect password." });
+        }
+
         console.log("Finished.");
         return callback(null, user);
       });
