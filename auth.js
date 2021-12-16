164ebaafedfa4196ab438fa2acd9a2ebca0983fb
tadpoleann <laesybab@gmail.com>
2021-12-15 22:55:32 -0800

    added jsdoc comments

--- auth.js
+++ auth.js
@@ -1,18 +1,24 @@
-const jwtSecret = "my_secret";
+const jwtSecret = "my_secret"; //must be the same key used in JWTStrategy
 
 const jwt = require("jsonwebtoken"),
   passport = require("passport");
 require("./passport");
 
+/**
+ * Function to generate authentication token with expiration and algorithm settings
+ * @param  {string} user
+ */
 let generateJWTToken = (user) => {
   return jwt.sign(user, jwtSecret, {
-    subject: user.Username,
-    expiresIn: "7d", //toke nwill expire in 7 days
+    subject: user.Username, //jtw encodes username
+    expiresIn: "7d", //token will expire in 7 days
     algorithm: "HS256", //the algorithm used to "sign" or encode the values of the JWT
   });
 };
 
-// POST login
+/**
+ * API call to login endpoint, authenticates user after entering login credentials
+ */
 module.exports = (router) => {
   router.post("/login", (req, res) => {
     passport.authenticate("local", { session: false }, (error, user) => {
